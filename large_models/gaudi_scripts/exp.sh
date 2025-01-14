011aaa44477bf8ffe629f6788436401e359d0a2b
siihwanpark <sihwan.park@mli.kaist.ac.kr>
2025-01-15 03:13:31 +0900

    Feat: update exp.sh for Gaudi experiments

--- large_models/gaudi_scripts/exp.sh
+++ large_models/gaudi_scripts/exp.sh
@@ -3,7 +3,7 @@ device=$1
 if [ $device -eq 0 ]; then
     for task in SST2 RTE CB BoolQ WSC WIC Copa MultiRC ReCoRD SQuAD DROP;do
         for lr in 1e-07 2e-07 5e-07;do
-            bash scripts/run.sh MeZO-SGD $task\
+            bash gaudi_scripts/run.sh MeZO-SGD $task\
                 --learning_rate $lr --max_steps 2000 --momentum 0.0 --save_perturbations
         done
     done
@@ -11,7 +11,7 @@ if [ $device -eq 0 ]; then
 elif [ $device -eq 1 ]; then
     for task in SST2 RTE CB BoolQ WSC WIC Copa MultiRC ReCoRD SQuAD DROP;do
         for lr in 1e-06 2e-06;do
-            bash scripts/run.sh MeZO-SGD $task\
+            bash gaudi_scripts/run.sh MeZO-SGD $task\
                 --learning_rate $lr --max_steps 2000 --momentum 0.0 --save_perturbations\
                 --sparse_perturbation --sparse_perturbation_type random --gradient_sparsity 0.75
         done
@@ -20,7 +20,7 @@ elif [ $device -eq 1 ]; then
 elif [ $device -eq 2 ]; then
     for task in SST2 RTE CB BoolQ WSC WIC Copa MultiRC ReCoRD SQuAD DROP;do
         for lr in 5e-06 1e-05;do
-            bash scripts/run.sh MeZO-SGD $task\
+            bash gaudi_scripts/run.sh MeZO-SGD $task\
                 --learning_rate $lr --max_steps 2000 --momentum 0.0 --save_perturbations\
                 --sparse_perturbation --sparse_perturbation_type random --gradient_sparsity 0.75
         done
@@ -29,7 +29,7 @@ elif [ $device -eq 2 ]; then
 elif [ $device -eq 3 ]; then
     for task in SST2 RTE CB BoolQ WSC WIC;do
         for lr in 1e-07 2e-07 5e-07;do
-            bash scripts/run.sh MeZO-SGD $task\
+            bash gaudi_scripts/run.sh MeZO-SGD $task\
                 --learning_rate $lr --max_steps 2000 --momentum 0.0 --save_perturbations\
                 --sparse_perturbation --sparse_perturbation_type scale --gradient_sparsity 0.75
         done
@@ -37,7 +37,7 @@ elif [ $device -eq 3 ]; then
 elif [ $device -eq 4 ]; then
     for task in Copa MultiRC ReCoRD SQuAD DROP;do
         for lr in 1e-07 2e-07 5e-07;do
-            bash scripts/run.sh MeZO-SGD $task\
+            bash gaudi_scripts/run.sh MeZO-SGD $task\
                 --learning_rate $lr --max_steps 2000 --momentum 0.0 --save_perturbations\
                 --sparse_perturbation --sparse_perturbation_type scale --gradient_sparsity 0.75
         done
@@ -46,7 +46,7 @@ elif [ $device -eq 4 ]; then
 elif [ $device -eq 5 ]; then
     for task in SST2 RTE CB BoolQ WSC WIC Copa MultiRC ReCoRD SQuAD DROP;do
         for lr in 1e-07 2e-07 5e-07;do
-            bash scripts/run.sh MeZO-SGD $task\
+            bash gaudi_scripts/run.sh MeZO-SGD $task\
                 --learning_rate $lr --max_steps 2000 --momentum 0.0 --save_perturbations\
                 --lozo_perturbation --rank_r 2 --lowrank_step_interval 100
         done
@@ -54,7 +54,7 @@ elif [ $device -eq 5 ]; then
 elif [ $device -eq 6 ]; then
     for task in SST2 RTE CB BoolQ WSC WIC Copa MultiRC ReCoRD SQuAD DROP;do
         for lr in 2e-06 5e-06 1e-05 2e-05 5e-05 1e-04;do
-            bash scripts/run.sh MeZO-SGD $task\
+            bash gaudi_scripts/run.sh MeZO-SGD $task\
                 --learning_rate $lr --max_steps 2000 --momentum 0.0 --save_perturbations\
                 --bcd --bcd_order random --bcd_interval 100
         done
@@ -65,7 +65,7 @@ elif [ $device -eq 7 ]; then
         for lr in 5e-05 1e-04 2e-04;do
             for K in 50 100;do
                 for order in random acending;do
-                    bash scripts/run.sh $device MeZO-Adam $task\
+                    bash gaudi_scripts/run.sh $device MeZO-Adam $task\
                         --learning_rate $lr --max_steps 20000\
                         --badam --badam_K $K --badam_ordering $order --state_flush\
                         --v_t_logging_steps 10
