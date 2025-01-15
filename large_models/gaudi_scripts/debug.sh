134a0cd21e0532ecc61ee4af5693f6b858d13d16
siihwanpark <sihwan.park@mli.kaist.ac.kr>
2025-01-15 14:52:56 +0900

    Fix: Fix

--- large_models/gaudi_scripts/debug.sh
+++ large_models/gaudi_scripts/debug.sh
@@ -22,11 +22,11 @@
 # bash scripts/run.sh $device MeZO-Adam\
 #     --p_scaled_perturbation --learning_rate 5e-05 --eval_steps 5
 
-export PT_HPU_LAZY_MODE=0
-export PT_HPU_GPU_MIGRATION=1
+# export PT_HPU_LAZY_MODE=0
+# export PT_HPU_GPU_MIGRATION=1
 
-bash gaudi_scripts/run.sh MeZO-SGD Copa\
-	--learning_rate 1e-06 --max_steps 10000 --save_perturbation --sparse_perturbation --sparse_perturbation_type scale --gradient_sparsity 0.75
+bash gaudi_scripts/run.sh MeZO-SGD SST2\
+	--learning_rate 1e-06 --max_steps 10000 --save_perturbation --sparse_perturbation --sparse_perturbation_type random --gradient_sparsity 0.75
 #	--learning_rate 1e-06 --max_steps 10000 --sparse_perturbation --sparse_perturbation_type random --gradient_sparsity 0.75 --save_perturbations
     # --learning_rate 1e-06 --max_steps 10000 --lozo_perturbation --rank_r 2 --lowrank_step_interval 100
     # --learning_rate 1e-6 --max_steps 10000 --bcd --bcd_ordering random --bcd_interval 100
