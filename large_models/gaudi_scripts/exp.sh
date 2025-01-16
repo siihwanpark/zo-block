24a6aef99216a3ead736de3ae68a02349c63d62a
siihwanpark <sihwan.park@mli.kaist.ac.kr>
2025-01-16 12:55:45 +0900

    Feat: update  for Gaudi experiments

--- large_models/gaudi_scripts/exp.sh
+++ large_models/gaudi_scripts/exp.sh
@@ -1,104 +1,63 @@
 device=$1
 
 if [ $device -eq 0 ]; then
-    for task in RTE BoolQ WIC Copa MultiRC ReCoRD SQuAD DROP;do
-        if [ $task == "BoolQ" ] || [ $task == "MultiRC" ] || [ $task == "ReCoRD" ] || [ $task == "SQuAD" ] || [ $task == "DROP" ];then
-            steps=1000
-        else
-            steps=2000
-        fi
-
-        for lr in 1e-07 2e-07;do
-            bash gaudi_scripts/run.sh MeZO-SGD $task\
-                --learning_rate $lr --max_steps $steps --momentum 0.0 --save_perturbations
-        done
+    for task in SST2 CB WSC SQuAD DROP;do
+        bash gaudi_scripts/run.sh MeZO-SGD $task\
+            --learning_rate 1e-07 --max_steps 20000 --momentum 0.0 --save_perturbations
     done
 
 elif [ $device -eq 1 ]; then
-    for task in RTE BoolQ WSC WIC Copa;do
-        if [ $task == "BoolQ" ] || [ $task == "MultiRC" ] || [ $task == "ReCoRD" ] || [ $task == "SQuAD" ] || [ $task == "DROP" ];then
-            steps=1000
-        else
-            steps=2000
-        fi
-
-        for lr in 1e-06 2e-06;do
-            bash gaudi_scripts/run.sh MeZO-SGD $task\
-                --learning_rate $lr --max_steps $steps --momentum 0.0 --save_perturbations\
-                --sparse_perturbation --sparse_perturbation_type random --gradient_sparsity 0.75
-        done
+    for task in RTE BoolQ WIC Copa MultiRC ReCoRD;do
+        bash gaudi_scripts/run.sh MeZO-SGD $task\
+            --learning_rate 1e-06 --max_steps 20000 --momentum 0.0 --save_perturbations
     done
 
 elif [ $device -eq 2 ]; then
-    for task in MultiRC ReCoRD SQuAD DROP;do
-        if [ $task == "BoolQ" ] || [ $task == "MultiRC" ] || [ $task == "ReCoRD" ] || [ $task == "SQuAD" ] || [ $task == "DROP" ];then
-            steps=1000
-        else
-            steps=2000
-        fi
-
-        for lr in 1e-06 2e-06;do
-            bash gaudi_scripts/run.sh MeZO-SGD $task\
-                --learning_rate $lr --max_steps $steps --momentum 0.0 --save_perturbations\
-                --sparse_perturbation --sparse_perturbation_type random --gradient_sparsity 0.75
-        done
+    for task in SST2 CB WSC SQuAD DROP;do
+        bash gaudi_scripts/run.sh MeZO-SGD $task\
+            --learning_rate 1e-07 --max_steps 20000 --momentum 0.0 --save_perturbations\
+            --lozo_perturbation --rank_r 2 --lowrank_step_interval 100
     done
 
 elif [ $device -eq 3 ]; then
-    for task in CB RTE BoolQ WSC WIC Copa;do
-        if [ $task == "BoolQ" ] || [ $task == "MultiRC" ] || [ $task == "ReCoRD" ] || [ $task == "SQuAD" ] || [ $task == "DROP" ];then
-            steps=1000
-        else
-            steps=2000
-        fi
-
-        for lr in 5e-07 1e-06;do
-            bash gaudi_scripts/run.sh MeZO-SGD $task\
-                --learning_rate $lr --max_steps $steps --momentum 0.0 --save_perturbations\
-                --sparse_perturbation --sparse_perturbation_type scale --gradient_sparsity 0.75
-        done
+    for task in RTE BoolQ WIC Copa MultiRC ReCoRD;do
+        bash gaudi_scripts/run.sh MeZO-SGD $task\
+            --learning_rate 1e-06 --max_steps 20000 --momentum 0.0 --save_perturbations\
+            --lozo_perturbation --rank_r 2 --lowrank_step_interval 100
     done
-elif [ $device -eq 4 ]; then
-    for task in MultiRC ReCoRD SQuAD DROP;do
-        if [ $task == "BoolQ" ] || [ $task == "MultiRC" ] || [ $task == "ReCoRD" ] || [ $task == "SQuAD" ] || [ $task == "DROP" ];then
-            steps=1000
-        else
-            steps=2000
-        fi
 
-        for lr in 5e-07 1e-06;do
-            bash gaudi_scripts/run.sh MeZO-SGD $task\
-                --learning_rate $lr --max_steps $steps --momentum 0.0 --save_perturbations\
-                --sparse_perturbation --sparse_perturbation_type scale --gradient_sparsity 0.75
-        done
+elif [ $device -eq 4 ]; then
+    for task in SST2 CB WSC SQuAD DROP;do
+        bash gaudi_scripts/run.sh MeZO-SGD $task\
+            --learning_rate 1e-05 --max_steps 20000 --momentum 0.0 --save_perturbations\
+            --bcd --bcd_ordering random --bcd_interval 100
     done
 
 elif [ $device -eq 5 ]; then
-    for task in SST2 RTE CB BoolQ WSC WIC Copa MultiRC ReCoRD SQuAD DROP;do
-        if [ $task == "BoolQ" ] || [ $task == "MultiRC" ] || [ $task == "ReCoRD" ] || [ $task == "SQuAD" ] || [ $task == "DROP" ];then
-            steps=1000
+    for task in RTE BoolQ WIC Copa MultiRC ReCoRD;do
+        bash gaudi_scripts/run.sh MeZO-SGD $task\
+            --learning_rate 1e-04 --max_steps 20000 --momentum 0.0 --save_perturbations\
+            --bcd --bcd_ordering random --bcd_interval 100
+    done
+
+elif [ $device -eq 6 ]; then
+    for task in SST2 RTE CB BoolQ WSC WIC Copa MultiRC ReCoRD SQuAD;do
+        if [ $task == "RTE" ] || [ $task == "BoolQ" ] || [ $task == "WIC" ] || [ $task == "Copa" ] || [ $task == "MultiRC" ] || [ $task == "ReCoRD" ];then
+            lrs=(1e-07 1e-06)
         else
-            steps=2000
+            lrs=(1e-06 1e-05)
         fi
 
-        for lr in 1e-07 2e-07;do
+        for lr in ${lrs[@]};do
             bash gaudi_scripts/run.sh MeZO-SGD $task\
-                --learning_rate $lr --max_steps $steps --momentum 0.0 --save_perturbations\
-                --lozo_perturbation --rank_r 2 --lowrank_step_interval 100
+                --learning_rate $lr --max_steps 1000 --momentum 0.0 --save_perturbations\
+                --sparse_perturbation --sparse_perturbation_type scale --gradient_sparsity 0.75
         done
     done
-elif [ $device -eq 6 ]; then
-    for task in RTE CB BoolQ WSC WIC Copa MultiRC ReCoRD SQuAD DROP;do
-        if [ $task == "BoolQ" ] || [ $task == "MultiRC" ] || [ $task == "ReCoRD" ] || [ $task == "SQuAD" ] || [ $task == "DROP" ];then
-            steps=1000
-        else
-            steps=2000
-        fi
 
-        for lr in 2e-06 5e-06 1e-05;do
-            bash gaudi_scripts/run.sh MeZO-SGD $task\
-                --learning_rate $lr --max_steps $steps --momentum 0.0 --save_perturbations\
-                --bcd --bcd_order random --bcd_interval 100
-        done
+    for lr in 1e-07 1e-06;do
+        bash gaudi_scripts/run.sh MeZO-SGD DROP\
+            --learning_rate $lr --max_steps 1000 --momentum 0.0\
+            --sparse_perturbation --sparse_perturbation_type scale --gradient_sparsity 0.75
     done
 fi
