686252745fa5d27e9b250595a3f883d7bfa95c74
siihwanpark <sihwan.park@mli.kaist.ac.kr>
2025-01-17 17:00:52 +0900

    Fix: use  instead of

--- large_models/gaudi_scripts/run.sh
+++ large_models/gaudi_scripts/run.sh
@@ -7,20 +7,23 @@ trainer=$1
 task=$2
 shift 2
 
-# Default values
-MODEL="facebook/opt-13b"
-BS=16
-EPS=1e-3
-TRAIN=1000
-DEV=500
-EVAL=1000
-STEPS=10000
-EVAL_STEPS=500
-MODE="ft"
 TASK=$task
-SEED=0
 Trainer=$trainer
-SAVE_STEPS=1000
+
+MODEL=${MODEL:-facebook/opt-13b}
+MODEL_NAME=(${MODEL//\// })
+MODEL_NAME="${MODEL_NAME[-1]}"
+
+BS=${BS:-16}
+LR=${LR:-1e-7}
+EPS=${EPS:-1e-3}
+SEED=${SEED:-0}
+TRAIN=${TRAIN:-1000}
+DEV=${DEV:-500}
+EVAL=${EVAL:-1000}
+STEPS=${STEPS:-20000}
+EVAL_STEPS=${EVAL_STEPS:-500}
+MODE=${MODE:-ft}
 
 # Additional arguments based on mode
 case $MODE in
@@ -49,56 +52,48 @@ case $TASK in
         ;;
 esac
 
+# Generate tag
+TAG="$Trainer-$MODE-$STEPS-$BS-$LR-$EPS-$SEED-$STEP_INTERVAL-$RANK"
 
-LR_LIST=(1e-5)
-BS_LIST=(16)
+# Print configuration
+echo "================= Configuration ================="
+echo "Model:           $MODEL"
+echo "Trainer:         $Trainer"
+echo "Steps:           $STEPS"
+echo "Eval steps:      $EVAL_STEPS"
+echo "Mode:            $MODE"
+echo "Task:            $TASK"
+echo "Batch size:      $BS"
+echo "Learning rate:   $LR"
+echo "EPS:             $EPS"
+echo "Seed:            $SEED"
+echo "Train/Eval steps: $STEPS/$EVAL_STEPS"
+echo "Mode:            $MODE"
+echo "Extra args:      $EXTRA_ARGS $TASK_ARGS"
+echo "Rank:            $RANK"
+echo "Step interval:   $STEP_INTERVAL"
+echo "Tag:             $TAG"
+echo "Num Sampling:    $NUM_SAMPLING"
+echo "================================================="
 
-for BS in ${BS_LIST[@]}; do
-    for LR in "${LR_LIST[@]}"; do
-        # Generate tag
-        TAG="$Trainer-$MODE-$STEPS-$BS-$LR-$EPS-$SEED-$STEP_INTERVAL-$RANK"
-
-        # Print configuration
-        echo "================= Configuration ================="
-        echo "Model:           $MODEL"
-        echo "Trainer:         $Trainer"
-        echo "Steps:           $STEPS"
-        echo "Eval steps:      $EVAL_STEPS"
-        echo "Mode:            $MODE"
-        echo "Task:            $TASK"
-        echo "Batch size:      $BS"
-        echo "Learning rate:   $LR"
-        echo "EPS:             $EPS"
-        echo "Seed:            $SEED"
-        echo "Train/Eval steps: $STEPS/$EVAL_STEPS"
-        echo "Mode:            $MODE"
-        echo "Extra args:      $EXTRA_ARGS $TASK_ARGS"
-        echo "Rank:            $RANK"
-        echo "Step interval:   $STEP_INTERVAL"
-        echo "Tag:             $TAG"
-        echo "Num Sampling:    $NUM_SAMPLING"
-        echo "================================================="
-
-        # Execute script
-        python run_gaudi.py \
-            --model_name $MODEL \
-            --task_name $TASK \
-            --output_dir result/$TASK-${MODEL}-$TAG --tag $TAG \
-            --train_set_seed $SEED \
-            --num_train $TRAIN --num_dev $DEV --num_eval $EVAL \
-            --logging_steps 1 \
-            --max_steps $STEPS \
-            --trainer $Trainer --load_float16 \
-            --learning_rate $LR --zo_eps $EPS \
-            --per_device_train_batch_size $BS \
-            --lr_scheduler_type "constant" \
-            --evaluation_strategy steps --save_strategy no \
-            --eval_steps $EVAL_STEPS \
-            --train_as_classification \
-            --max_grad_norm 0.0 \
-            --early_stop\
-            $EXTRA_ARGS \
-            $TASK_ARGS \
-            $@
-    done
-done
+# Execute script
+python run_gaudi.py \
+    --model_name $MODEL \
+    --task_name $TASK \
+    --output_dir result/$TASK-${MODEL}-$TAG --tag $TAG \
+    --train_set_seed $SEED \
+    --num_train $TRAIN --num_dev $DEV --num_eval $EVAL \
+    --logging_steps 1 \
+    --max_steps $STEPS \
+    --trainer $Trainer --load_float16 \
+    --learning_rate $LR --zo_eps $EPS \
+    --per_device_train_batch_size $BS \
+    --lr_scheduler_type "constant" \
+    --evaluation_strategy steps --save_strategy no \
+    --eval_steps $EVAL_STEPS \
+    --train_as_classification \
+    --max_grad_norm 0.0 \
+    --early_stop\
+    $EXTRA_ARGS \
+    $TASK_ARGS \
+    $@
